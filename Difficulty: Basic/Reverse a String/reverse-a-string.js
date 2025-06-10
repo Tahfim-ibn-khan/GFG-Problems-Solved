48b6197f56636cfea52a78ac7000a081361a02ca
Tahfim Ibn Khan <tahfimibnkhan123@gmail.com>
2025-06-10 18:23:37 +0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse a String/reverse-a-string.js
@@ -0,0 +1,17 @@
+// User function Template for javascript
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+class Solution {
+
+    reverseString(s) {
+        let str="";
+        for(let i=s.length-1;i>=0;i--){
+            str+=s[i];
+        }
+        return str;
+    }
+}
