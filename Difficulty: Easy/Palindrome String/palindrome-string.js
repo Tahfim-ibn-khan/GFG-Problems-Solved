f058b9662b85c95e39888a8ae4bea9bb362c3cc1
Tahfim Ibn Khan <tahfimibnkhan123@gmail.com>
2025-06-10 17:01:05 +0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome String/palindrome-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+class Solution {
+    // Function to check if a given string is a palindrome.
+    isPalindrome(s) {
+        let left=0;
+        let right=s.length-1;
+        while(left<right){
+            if(!(s[left]==s[right])) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
