0a75c898b6ae0e1b333d05be80d71983da24b293
Tahfim Ibn Khan <tahfimibnkhan123@gmail.com>
2025-06-11 13:43:15 +0600

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Third largest element/third-largest-element.js
+++ Difficulty: Easy/Third largest element/third-largest-element.js
@@ -8,11 +8,11 @@ class Solution {
     thirdLargest(arr) {
         // your code here
         arr= arr.sort((a,b)=>b-a);
-        const unique=new Set(arr);
         if(arr.length<=2){
             return -1;
         }
         return arr[2];
+        // const unique=new Set(arr);
         // let counter=0;
         // for(const val of unique){
         //     counter+=1;
