cd9d21d34e8eb2ad350841e5303e95cf29a64f4f
Tahfim Ibn Khan <tahfimibnkhan123@gmail.com>
2025-06-11 14:11:47 +0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check if array is sorted/check-if-array-is-sorted.js
@@ -0,0 +1,11 @@
+class Solution {
+    arraySortedOrNot(arr) {
+        // code here
+        for(let i=0; i<arr.length-1; i++){
+            if(!(arr[i]<=arr[i+1])){
+                return false;
+            }
+        }
+        return true;
+    }
+}
